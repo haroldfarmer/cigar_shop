7d156c38c5e82a9b885a4692852eadf1489e9326
Harold Farmer <haroldfarmer30@gmail.com>
2023-03-10 19:29:47 -0500

    api connects to db

--- js/cigars.js
+++ js/cigars.js
@@ -2,7 +2,7 @@ let num_of_cigars= 10;
 
 
 function get_cigar_data() {
-    const promise = axios.get('http://127.0.0.1:5000/get');
+    const promise = axios.get('http://127.0.0.1:8000/get_cigar_data');
     const dataPromise = promise.then((response) => response.data)
     return dataPromise
     
