0bf429cca7ea50bb34e9a04f919ba1f6009c3dac
root <root@DESKTOP-N202CJF.localdomain>
2025-01-03 20:09:57 -0500

    temporarily store data because db isn't up

--- js/cigars.js
+++ js/cigars.js
@@ -1,40 +1,47 @@
-let num_of_cigars= 10;
+let numOfCigarsMap = new Map()
+numOfCigarsMap.set('first',10);
+numOfCigarsMap.set('second',10);
+numOfCigarsMap.set('third',10);
+numOfCigarsMap.set('fourth',10);
+numOfCigarsMap.set('fifth',10);
+numOfCigarsMap.set('sixth',10);
 
 
 function get_cigar_data() {
     const promise = axios.get('http://127.0.0.1:8000/get_cigar_data');
     const dataPromise = promise.then((response) => response.data)
     return dataPromise
-    
+
 }
 
-function get_price() {
-    get_cigar_data()
+function getPrice() {
+    getCigarData()
         .then(data => {
             price = data[0][3].toString();
-            document.getElementById("price").innerText = 'Price: $' + price; 
+            document.getElementById("price").innerText = 'Price: $' + price;
         })
         .catch(err => console.log(err))
 
-    
 }
 
-function cigar_bought() {
-
-    num_of_cigars--;
-    if (num_of_cigars < 1) {
-        document.getElementById("num_cigars").innerText = 'Price: OUT OF ORDER';
+function cigarBought(id) {
+    let numOfCigars = numOfCigarsMap.get(id)
+    numOfCigars--;
+    numOfCigarsMap.set(id,numOfCigars)
+    console.log(numOfCigars)
+    if (numOfCigars < 1) {
+        document.getElementById(id).innerText = 'Price: OUT OF ORDER';
     }
     else {
-        let str_num_of_cigars = num_of_cigars.toString();
-        document.getElementById("num_cigars").innerText = 'Price: $' + str_num_of_cigars;
+        let str_num_of_cigars = numOfCigars.toString();
+        document.getElementById(id).innerText = 'Amount ' + str_num_of_cigars;
     }
 
 }
 
 window.onload = (event) => {
     console.log('The page has fully loaded');
-    get_price();
+    getPrice();
 };
 
 
